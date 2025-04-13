052c863fa1aa0897597aa87780804306c97c37f5
Andre Chatzistamatiou <andre.chatzistamatiou@gmail.com>
2025-04-13 21:40:40 +0200

    update

--- src/dexes/aftermath/pool_manager.ts
+++ src/dexes/aftermath/pool_manager.ts
@@ -56,7 +56,7 @@ export class PoolManagerAftermath extends PoolManagerWithClient {
                         const pool = response.filter((p) => p.objectId == address)[0];
                         pool.stats = stats[j];
                     });
-                    sleep(3000);
+                    sleep(5000);
                 }
                 this.last_call_aftermath_api = Date.now();
 
