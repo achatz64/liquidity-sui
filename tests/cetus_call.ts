386173f2607ef5a27c78ec0ae0bd5b29f2f25587
Andre Chatzistamatiou <andre.chatzistamatiou@gmail.com>
2025-10-31 08:29:58 +0100

    update

--- tests/cetus_call.ts
+++ tests/cetus_call.ts
@@ -1,12 +1,30 @@
-async function cetus_pools(limit=30, offset=0) {
-    const response = await (await fetch(`https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-vol&limit=${limit}&offset=${offset}`, {
-        "headers": {
-          "accept": "*/*",
-        },
-        "body": null,
-        "method": "GET"
-      })).json();
-    console.log(response);
+// async function cetus_pools_old(limit=30, offset=0) {
+//     const response = await (await fetch(`https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-vol&limit=${limit}&offset=${offset}`, {
+//         "headers": {
+//           "accept": "*/*",
+//         },
+//         "body": null,
+//         "method": "GET"
+//       })).json();
+//     console.log(response);
+// }
+
+async function cetus_pools(limit=30, offset=30) {
+  const payload = {
+    filter: "verified",
+    sortBy: "vol",
+    sortOrder: "desc",
+    limit,
+    offset
+  }
+  const response = await (await fetch("https://api-sui.cetus.zone/v3/sui/clmm/stats_pools", {
+    "headers": {
+      "accept": "*/*"
+    },
+    "body": JSON.stringify(payload),
+    "method": "POST"
+  })).json();
+  console.log(response);
 }
 
 cetus_pools();
