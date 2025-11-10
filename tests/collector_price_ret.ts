5621a4c8d60fca92419ed0d177528ef008bbaf37
Andre Chatzistamatiou <andre.chatzistamatiou@gmail.com>
2025-11-10 10:48:25 +0100

    update

--- tests/collector_price_ret.ts
+++ tests/collector_price_ret.ts
@@ -1,5 +1,7 @@
 import { DataRequest, PriceRetrieval, ResponsePriceRetrieval } from "../src/defs/collector";
 
+const print_pool_price = "0x90dbf049993b40ecc5bc473414e3f61fae69436a1d8594034b49ff708d47de23";
+
 async function call(url: string, arg: DataRequest): Promise<ResponsePriceRetrieval> {
     const request: ResponsePriceRetrieval = await (await fetch(url, {
             "headers": {
@@ -22,10 +24,16 @@ setInterval(async () => {
         if (new_current.update) {
             current.timestamp = new_current.timestamp
             current.data = new_current.data!
+            if (current.data[print_pool_price]) {
+                console.log(current.data[print_pool_price]);
+            }
+            else {
+                console.log(`${print_pool_price} not found in ${Object.keys(current.data).join(",")}`)
+            }
             console.log((Date.now()/1000).toFixed(1) + " update")
         }
     }
     catch (error) {
         console.log(error);
     }
-}, 100)
+}, 1000)
